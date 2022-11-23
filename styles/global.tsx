5737848f9eb5cee9684af5d5faeca6bf750f896c
Dhiab, Mohamed Amine <mohamedamine.dhiab@dai-labor.de>
2022-11-23 02:31:51 +0100

    fixed favicon :bug:

--- styles/global.tsx
+++ styles/global.tsx
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Alfa Slab One';
         font-size:2rem;
         --light-violet:#E4E4F2;
-        --blue:#113A8C;
+        --blue:#113A8F;
         --dark-blue:#1D2840;
         --orange:#F2A172;
         --dark-pink:#BF95A9;
